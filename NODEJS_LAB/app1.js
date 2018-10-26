defd556ec90cc54d43e872314efe183c2c9ba1dc
mastah2003 <44333580+mastah2003@users.noreply.github.com>
2018-10-26 23:29:36 +0200

    Wykonałem pracę domową.

--- NODEJS_LAB/app1.js
+++ NODEJS_LAB/app1.js
@@ -1,18 +1,22 @@
 console.log('start app1');
 
 
-const arg = process.argv[2]; // dane wprowadzone jako argument w konsoli
+//onst arg = process.argv[2]; // dane wprowadzone jako argument w konsoli
 
+const myMath = require('./matematyczny'); // taki include , root
+const a= parseInt(process.argv[2]);       // zamiana na int
+const b= parseInt(process.argv[3]);
+// parseFloat - zamiana na float
 
-
-if (arg)
-
+let sum = 0;
+if (a && b)
 {
-        console.log('abc' + arg);
+      sum =  myMath.add(a,b);
+        console.log('sum equals: ' + sum);
 }
 else
 {
-        console.log('abc');
+        console.log('type 2 numbers as args');
 }
 
-// zrob operację matematyczna, podaj 2 argumenty i wydaj sumę. w konsoli
+// zrob operację matematyczna, podaj 2 argumenty i wydaj sumę. w konsoli, zapisz to rowniez w pliku
